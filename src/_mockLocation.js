e22cb61678cbf21acccea70e84f1da59d405dfe3
Brian Marin Silva <marinbrian.as@gmail.com>
2023-03-10 15:13:40 -0600

    Requestion Location Permissions, Reading the location, building out location architecture and context, adding live location updates, and drawing the position indicator

---
+++ src/_mockLocation.js
@@ -0,0 +1,26 @@
+// Mock location data for testing since the iOS simulator can be buggy
+import * as Location from 'expo-location';
+
+const tenMetersWithDegrees = 0.0001;
+
+const getLocation = (increment) => ({
+  timestamp: 10000000,
+  coords: {
+    speed: 0,
+    headaing: 0,
+    accuracy: 5,
+    altitudeAccuracy: 5,
+    altitude: 5,
+    longitude: -122.0312186 + increment * tenMetersWithDegrees,
+    latitude: 37.33233141 + increment * tenMetersWithDegrees,
+  },
+});
+
+let counter = 0;
+setInterval(() => {
+  Location.EventEmitter.emit('Expo.locationChanged', {
+    watchId: Location._getCurrentWatchId(),
+    location: getLocation(counter),
+  });
+  counter++;
+}, 1000);
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,23 @@
-import React from 'react';
+// import '../_mockLocation';
+import React, { useContext } from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { Text } from 'react-native-elements';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 
-const TrackCreateScreen = () => (
-  <SafeAreaView forceInset={{ top: 'always' }}>
-    <Text h2>Create a Track</Text>
-    <Map />
-  </SafeAreaView>
-);
+const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
+  const [err] = useLocation(addLocation);
+
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h2>Create a Track</Text>
+      <Map />
+      {err ? <Text>Please enable location services</Text> : null}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({});
 
