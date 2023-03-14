c8a9ef673e86312e489302140504c79bd2fe64ac
Brian Marin Silva <marinbrian.as@gmail.com>
2023-03-14 14:47:41 -0500

    Adding the tracking of the run, track management, automated authentication, and automatic form reset/navigation

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,19 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: 'https://aa7b-2601-2c6-4500-3fd0-b0ef-9b95-9110-4984.ngrok.io',
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+
+export default instance;
--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import { Context as LocationContext } from '../context/LocationContext';
 import Spacer from './Spacer';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
   const {
@@ -10,20 +11,27 @@ const TrackForm = () => {
     stopRecording,
     changeName,
   } = useContext(LocationContext);
+  const [saveTrack] = useSaveTrack();
 
   return (
     <>
       <Spacer />
-      <Input value={name} onChange={changeName} placeholder="Enter name" />
+      <Input value={name} onChangeText={changeName} placeholder="Enter name" />
       <Spacer>
         {recording ? (
           <Button title="Stop" onPress={stopRecording} />
         ) : (
           <Button title="Start Recording" onPress={startRecording} />
         )}
+        <Spacer />
+        {!recording && locations.length ? (
+          <Button title="Save Recording" onPress={saveTrack} />
+        ) : null}
       </Spacer>
     </>
   );
 };
 
 export default TrackForm;
+
+// Lesson 266, there is some error in the name value acceptance for some reason but code matches
