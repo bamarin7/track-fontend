f16a5863e7b865b2365333ce991d2c3efc271d7a
Brian Marin Silva <marinbrian.as@gmail.com>
2023-03-14 12:53:00 -0500

    Adding Track Form wire up, fixing bug in UseEffects by adding the UseCallback hook, and overall clean up including avoiding stale references

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,15 +1,29 @@
+import { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
+import { Context as LocationContext } from '../context/LocationContext';
 import Spacer from './Spacer';
 
-const TrackForm = () => (
-  <>
-    <Spacer>
-      <Input placeholder="Enter name" />
-    </Spacer>
-    <Spacer>
-      <Button title="Start Recording" />
-    </Spacer>
-  </>
-);
+const TrackForm = () => {
+  const {
+    state: { name, recording, locations },
+    startRecording,
+    stopRecording,
+    changeName,
+  } = useContext(LocationContext);
+
+  return (
+    <>
+      <Spacer />
+      <Input value={name} onChange={changeName} placeholder="Enter name" />
+      <Spacer>
+        {recording ? (
+          <Button title="Stop" onPress={stopRecording} />
+        ) : (
+          <Button title="Start Recording" onPress={startRecording} />
+        )}
+      </Spacer>
+    </>
+  );
+};
 
 export default TrackForm;
